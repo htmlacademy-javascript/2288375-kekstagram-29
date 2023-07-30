0ec973ec16f34ba269702a1b44e9480f7cd6e66e
Keks <keks@htmlacademy.ru>
2023-07-30 21:31:05 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,9 @@
+import {data} from './data.js';
+import {renderPictures} from './pictures.js';
+import './range-slider.js';
+import './full-pictures.js';
+import './form.js';
+
+
+renderPictures(data);
+
